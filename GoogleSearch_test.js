96c8006dd123f49b64ee6abd48dac808dd60e822
Theodoroszeta <74838416+Theodoroszeta@users.noreply.github.com>
2021-04-13 22:35:12 +0200

    Update GoogleSearch_test.js

--- GoogleSearch_test.js
+++ GoogleSearch_test.js
@@ -1,11 +1,10 @@
 Feature('GoogleSearch');
 
 Scenario('TC1-Google Search', (I) => {
-    I.am0nPage('/');
-    I.seeElement('//body[1]/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]');
+    I.am0nPage('http://www.google.com');
+    I.seeElement('Google');
     I.fillField('//body[1]/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]', 'meteo');
     I.click('input')
-    I.wait(2);
 
 
 });
